add967181a74a88f87584b749939e3ae6cbb032c
yashginoya <yashginoya2003@gmail.com>
2024-04-08 15:39:57 +0530

    Added and Setup Prisma added prisma models added prisma singleton client added intial-profile util in lib

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,27 @@
+import { db } from "@/lib/db";
+import { intialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async () => {
+    const profile = await intialProfile();
+
+    const server = await db.server.findFirst({
+        where : {
+            members :{
+                some :{
+                    profileId : profile.id
+                }
+            }
+        }
+    })
+
+    if(server){
+        return redirect(`/servers/${server.id}`)
+    }
+
+    return <div>
+        Create a Server
+    </div>;
+}
+ 
+export default SetupPage;
